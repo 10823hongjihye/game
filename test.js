6572ac82bc351d9580649c6d117c79f17358c28b
10823hongjihye <hongjihye070309@gmail.com>
2025-09-24 18:03:46 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+class Unit{
+    constructor(name, hp, atk){
+        this.name = name;
+        this.hp = hp;
+        this.atk = atk;
+    }
+}
+
+
+class Monster extends Unit {
+    constructor (name, HP, atk){
+        super(name, hp, atk);
+    }
+    attack(monster){
+        monster.hp -= this.atk
+
+    }
+}
+
+class Hero extends Unit {
+    cosntructor(name, hp, atk, xp){
+        super(name, hp, atk);
+        this.xp = xp;
+    }
+}
+
+const monster1 = new Monster('슬라임',10,10);
